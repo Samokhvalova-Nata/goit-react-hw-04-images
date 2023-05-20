20dbc64e96a4cb24fc873614c7536d10e03bffbb
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-05-20 16:20:31 +0300

    Fixed pagination

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,35 +12,32 @@ export const App = () => {
   const [gallery, setGallery] = useState([]);
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
+  const [isLoading, setIsLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     if (query) fetchImages();
 
     async function fetchImages() {
-      setStatus('pending');
+      setIsLoading(true);
       try {
           const { hits, total, totalHits } = await fetchGalleryWithQuery(query, page);
 
           if (total === 0) {
               const error = new Error('Sorry, there are no images matching your search query.')
-              setStatus('rejected');
               setError(error);
               return;
           };
 
-        // setGallery(prevGallery => (page === 1 ? hits : [...prevGallery, ...hits]));
         setGallery(prev =>  [...prev, ...hits]);
         setTotalPages(Math.floor(totalHits / 12));
-        setStatus('resolved');
       } catch (error) {
-          setStatus('rejected');
-          setError(error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
       };
     };
   }, [page, query]);
-  
   
   const searchFormSubmitHandler = query => {
     setQuery(query);
@@ -54,10 +51,10 @@ export const App = () => {
   return (
       <Container>
         <Searchbar onSubmit={searchFormSubmitHandler} />
-        {status === 'pending' && <Loader />};
-        {status === 'rejected' && Notify.failure(`${error.message}`)};
-        {status === 'resolved' && <ImageGallery gallery={gallery} />}
-        {status === 'resolved' && page < totalPages && <Button onClick={loadMoreHandler}>Load more</Button>}
+        {isLoading && <Loader />};
+        {error && Notify.failure(`${error.message}`)};
+        { gallery.length > 0 && <ImageGallery gallery={gallery} />}
+        { gallery.length > 0 && page < totalPages && <Button onClick={loadMoreHandler}>Load more</Button>}
       </Container>
     );
 };
